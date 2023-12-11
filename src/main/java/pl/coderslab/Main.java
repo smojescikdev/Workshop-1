72b65f967e3e28830fb127597fdab3d4210076e5
smojescikdev <s.mojescik.dev@gmail.com>
2023-12-11 20:35:09 +0100

    Methode: readFromFile done

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,25 +1,48 @@
 package pl.coderslab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        String[][] array = new String[2][3];
+        readFromFile(array);
 
         String[] menu = new String[4];
-        Arrays.toString(menuArray(menu));
+        menu = menuArray(menu);
+        System.out.println(Arrays.toString(menu));
     }
 
     public static String[] menuArray(String[] menu) {
-        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
-        menu[0] = "add";
-        menu[1] = "remove";
-        menu[2] = "list";
-        menu[3] = "exit";
+        System.out.println(ConsoleColors.BLUE + "Proszę wybrać opcję:" + ConsoleColors.RESET);
+        menu[0] = "Dodaj";
+        menu[1] = "Usuń";
+        menu[2] = "Lista";
+        menu[3] = "Wyjdź";
 
-        for (int i = 0; i < menu.length; i++) {
-            System.out.println(menu[i]);
+        for (String menuItem : menu) {
+            System.out.println(menuItem);
         }
-        return null;
+        return menu;
+    }
+
+    public static void readFromFile(String[][] tasks) {
+        StringBuilder reading = new StringBuilder();
+        File file = new File("C:\\Users\\Mojescik\\IdeaProjects\\TaskManager\\src\\main\\resources\\tasks.csv");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                reading.append(scanner.nextLine()).append("\n");
+            }
+            // Tutaj możesz dalej przetwarzać odczytaną zawartość, np. podzielić na linie
+            // lub wykonać inne operacje zgodnie z potrzebami twojego programu
+            System.out.println("Zawartość pliku:");
+            System.out.println(reading.toString());
+        } catch (FileNotFoundException e) {
+            System.out.println("Plik nie został znaleziony. " + e);
+        }
     }
 }
