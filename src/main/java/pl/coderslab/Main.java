6232615d96d41c3d18871ad816709ef43e7b9877
smojescikdev <s.mojescik.dev@gmail.com>
2023-12-11 20:07:44 +0100

    Methode: menuArray done Added resources csv and new Class ConsoleColors.java

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,25 @@
 package pl.coderslab;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        String[] menu = new String[4];
+        Arrays.toString(menuArray(menu));
+    }
+
+    public static String[] menuArray(String[] menu) {
+        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        menu[0] = "add";
+        menu[1] = "remove";
+        menu[2] = "list";
+        menu[3] = "exit";
+
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(menu[i]);
+        }
+        return null;
     }
 }
